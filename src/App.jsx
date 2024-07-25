0c2266a7e5516dde370010011b38248f3d3076cd
guido2288 <guido2288@gmail.com>
2024-07-25 19:58:32 -0300

    init repo

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import ListShop from "./components/ListShop"
+
+
+function App() {
+
+
+  return (
+    <main>
+      <h2>Desserts</h2>
+      <ListShop />
+    </main>
+  )
+}
+
+export default App
